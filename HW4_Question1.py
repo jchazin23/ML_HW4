7ff329318b96debf31329d0203eb4114388cec75
jchazin23 <jchazin@gmail.com>
2016-04-18 18:41:35 -0400

    ALS Algorithm in part B is now working and producing a GOOD log likelihood chart.

--- HW4_Question1.py
+++ HW4_Question1.py
@@ -29,22 +29,28 @@ log_likelihood_theta = []
 u = 5 * np.random.rand(m, k)## initialize u_hat_i for each user: multiplied by 5 to mimic 1-5 rating scale
 v = 5 * np.random.rand(k, n)## initialize v_hat_j for each movie: multiplied by 5 to mimic 1-5 rating scale
 
-
+## in this iteration, I removed the mu from inside each summation, moving it outside to avoid
+## duplication (since it is not subscripted by i or j)
 for t in range(T):
     for i in range(m):
         for j in range(n):
-            u[i] = -(np.dot(v[:,j],v[:,j].T)**-1) * (-(b[i] + mu - a_i_j[i,j]))*v[:,j]
-            b[i] = -(np.dot(u[i],v[:,j]) + c[:,j] + mu - a_i_j[i,j])/n
+            u[i] = -(np.dot(v[:,j],v[:,j].T)**-1) * (-(b[i] - a_i_j[i,j]))*v[:,j]
+            b[i] = -(np.dot(u[i],v[:,j]) + c[:,j] - a_i_j[i,j])/n
+        u[i] -= mu
+        b[i] -= mu            
+            
     for j in range(n):
         for i in range(m):
-            v[:,j] = -(np.dot(u[i],u[i].T)**-1) * (-(b[i] + mu - a_i_j[i,j]))*u[i]
-            c[:,j] = -(np.dot(u[i],v[:,j]) + b[i] + mu - a_i_j[i,j])/m
+            v[:,j] = -(np.dot(u[i],u[i].T)**-1) * (-(b[i] - a_i_j[i,j]))*u[i]
+            c[:,j] = -(np.dot(u[i],v[:,j]) + b[i] - a_i_j[i,j])/m
+        v[:,j] -= mu
+        c[:,j] -= mu
     
     result = 0
     for i in range(m):
         for j in range(n):
-            result += np.dot(u[i],v[:,j]) + b[i] + c[:,j] + mu - a_i_j[i,j] 
-    result = (-0.5)*(result)**2
+            result += np.dot(u[i],v[:,j]) + b[i] + c[:,j] - a_i_j[i,j] 
+    result = (-0.5)*(result + mu)**2
            
     log_likelihood_theta.append(result)
 
